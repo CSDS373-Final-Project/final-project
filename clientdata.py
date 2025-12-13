484eff558dd04ce20c67661470db85d08353f365
Madison Kekic <mkekic@peach.cs.oberlin.edu>
2025-12-13 16:55:31 -0500

    added datasets

--- clientdata.py
+++ clientdata.py
@@ -6,5 +6,5 @@ Contains useful information for processing playlist data
 CLIENT_ID = 'a681eb56d3674a7dbbfc3fc2b61ab419'
 CLIENT_SECRET = '28109a4001d143d9bea31b0e331318c1'
 
-MADDY_URLS = ["https://open.spotify.com/playlist/3T6DZbhqA9u1jVs1zzprdl", "https://open.spotify.com/playlist/3ICBL89st1qGkwo6evDrhN", "https://open.spotify.com/playlist/7dKIzrS0qkRFFveqvXBRuX", "https://open.spotify.com/playlist/4OhzHBYO15lPncgJSDFuj0", "https://open.spotify.com/playlist/22t9fUJBHZ94c1mlISQREs"]
+MADDY_PLAYS = ["https://open.spotify.com/playlist/3T6DZbhqA9u1jVs1zzprdl", "https://open.spotify.com/playlist/3ICBL89st1qGkwo6evDrhN", "https://open.spotify.com/playlist/7dKIzrS0qkRFFveqvXBRuX", "https://open.spotify.com/playlist/4OhzHBYO15lPncgJSDFuj0", "https://open.spotify.com/playlist/22t9fUJBHZ94c1mlISQREs"]
 MAXANN_PLAYS = ["6SnJpsK99YBNy5wYgzGImM", "3oIRsiXz95PjucjxtqTd3r", "2MpZByaU1LJZoOo3rMjH1g"]
